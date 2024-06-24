fc1027dd6e617f3fba94af1bbeeb300ecfb06a9f
Seo-Faper <dswhdcks@gmail.com>
2024-06-25 03:51:11 +0900

    url 문제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import ThemeSwitcher from './components/ThemeSwitcher';
 import Editor from './components/Editor';
 import Viewer from './components/Viewer';
@@ -7,6 +7,7 @@ import './App.css';
 
 function App() {
     return (
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Router>
             <div className="main-container bg-white dark:bg-gray-900 min-h-screen transition-colors duration-250 relative">
                 <div className="flex items-center justify-between w-full p-4">
@@ -22,6 +23,7 @@ function App() {
                 </div>
             </div>
         </Router>
+        </BrowserRouter>
     );
 }
 
